bd559eceb975bf9f9d984070d9749bc12f315dc9
marioMazza <mariomazza3322@gmail.com>
2021-10-15 11:47:47 -0300

    pseudo-random functionality update

--- contracts/FrankensteinTexts.sol
+++ contracts/FrankensteinTexts.sol
@@ -2,13 +2,15 @@ pragma solidity ^0.8.0;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Frankies is ERC721 {
+contract FrankensteinTexts is ERC721 {
 
     uint private requestCounter;
 
     uint private submitCounter;
 
     bool private firstTime;
+
+    bool public green;
 
     address private victor;
 
@@ -29,9 +31,32 @@ contract Frankies is ERC721 {
       submitCounter = 0;
       victor = msg.sender;
       firstTime = true;
+      green = true;
     }
 
-    function requestText() public returns(bytes32) {
+    modifier isGreen() {
+      require(green == true);
+      _;
+    }
+
+    function _shuffle() private view returns(uint) {
+      return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, hashOrder[requestCounter], hashOrder[submitCounter]))) % 100;
+    }
+
+    function _dealer() private {
+      uint random = _shuffle();
+      bytes32 chosen = hashOrder[random];
+      hashOrder[random] = hashOrder[requestCounter];
+      hashOrder[requestCounter] = chosen;
+    }
+
+    function requestText() public isGreen() returns(bytes32) {
+      uint distance;
+      unchecked { distance = submitCounter - requestCounter; }
+      if(distance >= 100) {
+        green = false;
+      }
+      _dealer();
       uint rc = requestCounter;
       if(rc % 5 == 0) {
         if(firstTime == true) {
@@ -41,10 +66,10 @@ contract Frankies is ERC721 {
           firstTime = true;
         }
       }
-      for(uint i=0; i < frankies[hashOrder[rc]].writers.length; i++) {
-        require(msg.sender != frankies[hashOrder[rc]].writers[i]);
-      }
-      requestCounter++;
+      //for(uint i=0; i < frankies[hashOrder[rc]].writers.length; i++) {
+      //  require(msg.sender != frankies[hashOrder[rc]].writers[i]);
+      //}
+      unchecked { requestCounter++; }
       frankies[hashOrder[rc]].editSince = block.timestamp;
       return hashOrder[rc];
     }
@@ -60,11 +85,16 @@ contract Frankies is ERC721 {
           }
         } else {
           hashOrder[submitCounter] = newHash;
-          submitCounter++;
+          unchecked { submitCounter++; } 
         }
       } else {
         hashOrder[submitCounter] = oldHash;
-        submitCounter++;
+        unchecked { submitCounter++; }
+      }
+    uint distance;
+    unchecked { distance = submitCounter - requestCounter; }
+    if(green == false && 100 <= distance) {
+        green = true;
       }
     }
 }
